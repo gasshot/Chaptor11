8ac0115d03a3fa44ec2b9719d715694f84f30c80
gasshot <kanginoh@gmail.com>
2023-07-06 17:56:48 +0900

    dasf

--- ConstantOnTypeParameters/MainApp.cs
+++ ConstantOnTypeParameters/MainApp.cs
@@ -2,19 +2,19 @@
 
 namespace ConstantOnTypeParameters
 {
-    class StructArray<T> where T : struct
+    class StructArray<T> where T : struct // 형식 매개변수 구조체로 제한
     { 
-    public T[] Array { get; set; }
+    public T[] Array { get; set; } // 프로퍼티
         public StructArray(int size) // 생성자
         {
             Array = new T[size];
         }
     }
 
-    class RefArray<T> where T : class
+    class RefArray<T> where T : class // 형식 매개변수 클래스로 제한
     {
-        public T[] Array { get; set; }
-        public RefArray(int size)
+        public T[] Array { get; set; } // 프로퍼티
+        public RefArray(int size) // 생성자
         {
             Array = new T[size];
         }
@@ -22,10 +22,10 @@ namespace ConstantOnTypeParameters
 
     class Base { }
     class Derived : Base { }
-    class BaseArray<U> where U : Base
+    class BaseArray<U> where U : Base // Base에 상속 받는 형식 매개변수 제한
     {
-        public U[] Array { get; set; }
-        public BaseArray(int size)
+        public U[] Array { get; set; } // 프로퍼티
+        public BaseArray(int size) // 생성자
         {
             Array = new U[size];
         }
@@ -39,7 +39,7 @@ namespace ConstantOnTypeParameters
 
     class MainApp
     {
-        public static T CreateInstance<T>() where T : new()
+        public static T CreateInstance<T>() where T : new() // 
         {
             return new T();
         }
@@ -50,16 +50,27 @@ namespace ConstantOnTypeParameters
             a.Array[1] = 1;
             a.Array[2] = 2;
 
+            StructArray<float> a1 = new StructArray<float>(3); // string은 참조형식이라 불가능함(테스트)
+            a1.Array[0] = 0.5f;
+            a1.Array[1] = 1;
+            a1.Array[2] = 2;
+
+            RefArray<string> a2 = new RefArray<string>(3);
+            a2.Array[0] = "C#";
+            a2.Array[1] = "노답";
+            a2.Array[2] = "극혐";
+
             RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
             b.Array[0] = new StructArray<double>(5);
-            b.Array[0].Array[0] = 0.5;
-            b.Array[0].Array[1] = 0.55;
-            b.Array[1] = new StructArray<double>(10);
-            b.Array[2] = new StructArray<double>(1005);
+            b.Array[0].Array[0] = 0.5; // 테스트
+            b.Array[0].Array[1] = 0.55; // 테스트
+            b.Array[0].Array[2] = 0.555; // 테스트
+            b.Array[1] = new StructArray<double>(10); // double타입 크기 10의 배열을 b.Array[1]
+            b.Array[2] = new StructArray<double>(1005); // double타입 크기 1005의 배열을 b.Array[2]
 
             BaseArray<Base> c = new BaseArray<Base>(3);
-            c.Array[0] = new Base();
-            c.Array[1] = new Derived();
+            c.Array[0] = new Base(); // U 제한된 형식 매개변수 (Base)
+            c.Array[1] = new Derived(); // Derived는 Base에 상속
             c.Array[2] = CreateInstance<Base>();
 
             BaseArray<Derived> d = new BaseArray<Derived>(3);
